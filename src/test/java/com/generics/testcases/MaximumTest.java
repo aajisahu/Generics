bae68a56e24ccdc3e7c3efda6e0d1ec073d76d7c
aajisahu <aajisahu.com>
2021-10-11 00:07:50 +0530

    added: Refactored to Generic Method

--- src/test/java/com/generics/testcases/MaximumTest.java
+++ src/test/java/com/generics/testcases/MaximumTest.java
@@ -7,76 +7,19 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class MaximumTest {
+public class MaximumTest
+{
     @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        int actualResult = Maximum.testMaximum(12, 4, 6);
-        Assert.assertEquals(12, actualResult);
+    public void intMax() {
+        int assertValue = Maximum.getmaximum(10,20,5);
+        Assert.assertEquals(20,assertValue);
     }
-
-    @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        int actualResult = Maximum.testMaximum(250, 500, 61);
-        Assert.assertEquals(500, actualResult);
+    public void floatMax() {
+        float assertValue = Maximum.getmaximum(10f,20f,5f);
+        Assert.assertEquals(20,assertValue,0);
     }
-
-    @Test
-    public void given3Values_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        int actualResult = Maximum.testMaximum(480, 720, 1040);
-        Assert.assertEquals(1040, actualResult);
-    }
-
-    @Test
-    public void givenValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        int actualResult = Maximum.testMaximum(360,480,720,1040,2080);
-        Assert.assertEquals(2080, actualResult);
-    }
-
-    @Test
-    public void given3floatValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        float actualResult = Maximum.testMaximum(10.11f, 10.01f, 10.0f);
-        Assert.assertEquals(10.11f, actualResult ,0);
-    }
-
-    @Test
-    public void given3floatValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-        float actualResult = Maximum.testMaximum(101.1f, 1011.0f, 100f);
-        Assert.assertEquals(1011f, actualResult,0);
-    }
-
-    @Test
-    public void given3flaotValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        float actualResult = Maximum.testMaximum(1.0f, 8.0f,1024.0f);
-        Assert.assertEquals(1024f, actualResult,0);
-    }
-
-    @Test
-    public void givenfloatValues_WhenGetMaximum_ShouldReturnCorrectValue() {
-        float actualResult = Maximum.testMaximum(36.0f, 480.0f, 72.0f, 10.20f,20.80f);
-        Assert.assertEquals(480.0f, actualResult,0);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue1() {
-        String actualResult = Maximum.testMaximum("Peach", "Apple", "Banana");
-        Assert.assertEquals("Peach", actualResult);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue2() {
-       String  actualResult = Maximum.testMaximum("Apple", "Peach", "Banana");
-        Assert.assertEquals("Peach", actualResult);
-    }
-
-    @Test
-    public void given3StringValues_WhenGetMaximum_ShouldReturnCorrectValue3() {
-        String actualResult = Maximum.testMaximum("Apple", "Banana", "Peach");
-        Assert.assertEquals("Peach", actualResult);
-    }
-
-    @Test
-    public void givenStringValues_WhenGetMaximum_ShouldReturnCorrectValue() {
-        String actualResult = Maximum.testMaximum("Apple", "Peach", "Banana", "Mango","Strawberry");
-        Assert.assertEquals("Strawberry", actualResult);
+    public void stringMax() {
+        String assertValue = Maximum.getmaximum("abc","def","ghi");
+        Assert.assertEquals("ghi",assertValue);
     }
 }
