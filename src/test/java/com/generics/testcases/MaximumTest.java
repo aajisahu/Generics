1bb8d18a4f865181aa5a282f1dba36d27264edb0
aajisahu <aajisahu.com>
2021-10-10 23:56:17 +0530

    added:program to find greatest of strings

--- src/test/java/com/generics/testcases/MaximumTest.java
+++ src/test/java/com/generics/testcases/MaximumTest.java
@@ -1,23 +1,27 @@
 package com.generics.testcases;
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class MaximumTest
-{
+public class MaximumTest {
     @Test
-    public void stringMax1() {
-        String actualValue = Maximum.getmaximum("Aa","Bb","Cc");
-        Assert.assertEquals("Cc",actualValue);
+    public void intMax() {
+        int actualValue = Maximum.testMaximum(10, 20, 5);
+        Assert.assertEquals(20, actualValue);
     }
 
     @Test
-    public void stringMax2() {
-        String actualValue = Maximum.getmaximum("abc","ABC","def");
-        Assert.assertEquals("def", actualValue);
+    public void floatMax() {
+        float actualValue = Maximum.testMaximum(11f,52f,36f);
+        Assert.assertEquals(52f,actualValue,0);
+    }
+
+    @Test
+    public void stringMax() {
+        String actualValue = Maximum.testMaximum("ABC","def","xyz");
+        Assert.assertEquals("xyz",actualValue);
     }
 }
